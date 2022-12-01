d28813eb3f9107595f724b99301e55e64f2c14b2
Alae Chatar <s112520@ap.be>
2022-12-01 12:57:38 +0100

    start implementation of TileSet and Block (not complete)

---
+++ Map/Block.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDev_project.Map
+{
+    internal class Block
+    {
+        public enum BlockType { Sky, Floor1, Floor2, Lava, Grass}
+        public BlockType block { get; set; }
+        public Texture2D texture;
+
+        public Block(BlockType blockType)
+        {
+            if (blockType == BlockType.Sky)
+                block = BlockType.Sky;
+            if (blockType == BlockType.Floor1)
+                block = BlockType.Floor1;
+            if (blockType == BlockType.Floor2)
+                block = BlockType.Floor2;
+            if (blockType == BlockType.Lava)
+                block = BlockType.Lava;
+            if (blockType == BlockType.Grass)
+                block = BlockType.Grass;
+        }
+    }
+}
---
+++ Map/TileSet.cs
@@ -0,0 +1,59 @@
+﻿using GameDev_project.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDev_project.Map
+{
+    internal class TileSet : IGameObject
+    {
+        public List<Block> BlocksFirstLevel = new List<Block>();
+        public List<Block> BlocksFinalLevel = new List<Block>();
+        int[,] level1 = new int[,]
+        {
+            {0, 0},
+            {1, 0},
+            {0, 0},
+            {1, 0},
+            {0, 0},
+            {1, 0},
+            {0, 0},
+            {1, 0},
+            {0, 0},
+            {1, 0}
+        };
+
+        int[,] level2 = new int[,]
+        {
+            {0, 0},
+            {1, 0},
+            {0, 0},
+            {1, 0},
+            {0, 0},
+            {1, 0},
+            {0, 0},
+            {1, 0},
+            {0, 0},
+            {1, 0}
+        };
+
+        public TileSet()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
